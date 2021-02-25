dd74e8bf17b1cb9cd47bb67713ce6a09cfc4a75d
Alexandra Chicinas <alexandraschicinas@yahoo.com>
2021-02-25 20:44:32 +0200

    create html content

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # teams-networking
+Teams job related networking( linkedIn, github)
+
+##TO DO
+
+[x] create index.html file with standards;
+[x] create table that represents final design and structure( 1 project as example)
+[x] hint - table -> THEAD & TBODY & TFOOD TAGS
+[x]Table headers -> [Members, Project name, Project URL]
--- functions.js
+++ functions.js
@@ -1,7 +1,19 @@
 fetch ( "persons.json")
-.then(restonse => restonse.json())
-.then( response => console.log(response));
+.then(response => response.json())
+.then( function(persons) {
+    console.warn("done",persons);
+    showAllMembers(persons);
+});
 
-function showPersons() {
-    
+function showAllMembers(persons){	
+    let allTeams = persons.map( person => {
+        return `<td> ${person.members}</td>,
+                <td> ${person.projectName}</td>,
+                <td> ${person.projectUrl} </td>`
+    })
+    let allTeamsHtml = document.querySelector(".members td");
+    console.log(allTeamsHtml);
+    allTeamsHtml.innerHTML = allTeams.join(" ")
+   
 }
+showAllMembers(allMembersHtml);	
