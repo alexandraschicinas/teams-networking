d71b4bda36de51ae075cd6ffcbb1dbd1c19bae53
Alexandra Chicinas <alexandraschicinas@yahoo.com>
2021-03-04 22:00:02 +0200

    add team UI

--- functions.js
+++ functions.js
@@ -6,9 +6,10 @@ function getHtmlTeams(teams){
                     <td> ${team.members}</td>
                     <td> ${team.name}</td>
                     <td> ${team.url} </td>
+                    <td> &#9747; &#9998;</td>
                  </tr>`
                 
-    }).join("")
+    }).join("");
 }
 
 function showTeams(teams){	
@@ -18,12 +19,37 @@ function showTeams(teams){
     allTeamsHtml.innerHTML = html;
 }
 
-fetch ( "teams.json")
+fetch ("teams.json")
 .then(response => response.json())
 .then(teams => {
     allTeams = teams;
     showTeams(allTeams)
 });
+
+function addTeam(team) {
+    console.warn("team:" , );
+    fetch ( "add.json", {
+        method: "POST",
+        body:JSON.stringify(team)
+    })
+        .then(response => response.json())
+        .then(status => {
+            console.warn("saved", status);
+        });
+}
+
+function saveTeam() {
+    const members = document.querySelector("input[name = members]").value;
+    const name = document.querySelector("input[name = namep]").value;
+    const url = document.querySelector("input[name = url]").value;
+
+    const team = {
+    name: name,
+    members: members,
+    url: url
+};
+addTeam(team);
+}
 
 
 	
